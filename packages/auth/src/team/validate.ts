018421c01a6840bcf0329ae8144730edda84e5c5
Isla <5048549+islathehut@users.noreply.github.com>
2026-06-15 08:52:33 -0400

    Revert "Merge pull request #24 from TryQuiet/fix/private-channels-admin-only"
    This reverts commit 5a7f0885eb69be31608643b939412568665d75cb, reversing
    changes made to 26219c923a71ea0e09507be5207de3f6e7fd6c26.

--- packages/auth/src/team/validate.ts
+++ packages/auth/src/team/validate.ts
@@ -9,6 +9,7 @@ import {
   type TeamState,
   type TeamStateValidator,
   type TeamStateValidatorSet,
+  type ValidationArgs,
 } from './types.js'
 
 export const validate: TeamStateValidator = (previousState: TeamState, link: TeamLink, extendableLogger?: Logger) => {
@@ -113,11 +114,14 @@ const validators: TeamStateValidatorSet = {
   },
 
   /** Check for self-assigned roles that aren't in the allowed list set by the admin */
-  nonAdminsCanOnlyModifyCertainRoles(previousState: TeamState, link: TeamLink, extendableLogger: Logger) {
-    const logger = extendableLogger.extend('nonAdminsCanOnlyModifyCertainRoles')
+  canOnlySelfAddCertainRoles(previousState: TeamState, link: TeamLink, extendableLogger: Logger) {
+    const logger = extendableLogger.extend('canOnlySelfAddCertainRoles')
     if (link.body.type === 'ADD_MEMBER_ROLE') {
       const { userId: assigningUserId } = link.body
-      const { roleName } = link.body.payload
+      const { userId, roleName } = link.body.payload
+      if (userId !== assigningUserId) {
+        return VALID
+      }
       const metadata = select.getMetadata(previousState)
       if (metadata.selfAssignableRoles.includes(roleName)) {
         return VALID
@@ -125,11 +129,8 @@ const validators: TeamStateValidatorSet = {
       const role = select.role(previousState, roleName)
       if (role.createdBy === assigningUserId) {
         return VALID
-      }
-      if (select.memberIsAdmin(previousState, assigningUserId)) {
-        return VALID
-      }
-      return fail(`User ${assigningUserId} attempted to assign role ${roleName} illegally`, previousState, link, logger)
+      } 
+      return fail(`User ${userId} attempted to self-assign role ${roleName} illegally`, previousState, link, logger)
     }
     return VALID
   },
