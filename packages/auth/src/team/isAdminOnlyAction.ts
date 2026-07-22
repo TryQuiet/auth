489840c01663614391f70d193e12b81ae9aa42ff
Isla <5048549+islathehut@users.noreply.github.com>
2026-07-22 12:58:03 -0400

    Add role accidentally got reverted to non-admin

--- packages/auth/src/team/isAdminOnlyAction.ts
+++ packages/auth/src/team/isAdminOnlyAction.ts
@@ -14,7 +14,6 @@ export const isAdminOnlyActionType = (actionType: TeamAction['type']): boolean =
     'CHANGE_SERVER_KEYS',
     'ADMIT_MEMBER',
     'ADMIT_DEVICE',
-    'ADD_ROLE',
     'ADD_MEMBER_ROLE',
     'ADD_STATIC_ROLE',
     'ADD_MEMBER_STATIC_ROLE',
